7621a5e3bb34ba10638c838fd11289cfc16365e9
Agzam <agzamasadullaev@gmail.com>
2024-05-31 14:40:04 +0500

    create presentation layer

---
+++ app/src/main/java/com/bionickhand/composition/presentation/GameFragment.kt
@@ -0,0 +1,18 @@
+package com.bionickhand.composition.presentation
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.bionickhand.composition.R
+
+class GameFragment : Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_game, container, false)
+    }
+}
