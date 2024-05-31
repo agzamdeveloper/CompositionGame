0b031e4379a9a92ee1b6fa5b2588b4e5b610d84a
Agzam <agzamasadullaev@gmail.com>
2024-05-31 18:37:46 +0500

    create gameFinishedFragment and viewBinding

--- app/src/main/java/com/bionickhand/composition/presentation/WelcomeFragment.kt
+++ app/src/main/java/com/bionickhand/composition/presentation/WelcomeFragment.kt
@@ -5,14 +5,25 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import com.bionickhand.composition.R
+import com.bionickhand.composition.databinding.FragmentWelcomeBinding
 
 class WelcomeFragment : Fragment() {
+
+    private var _binding: FragmentWelcomeBinding? = null
+    private val binding: FragmentWelcomeBinding
+        get() = _binding ?: throw RuntimeException("FragmentWelcomeBinding == null")
+
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        return inflater.inflate(R.layout.fragment_welcome, container, false)
+    ): View {
+        _binding = FragmentWelcomeBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        _binding = null
     }
 }
