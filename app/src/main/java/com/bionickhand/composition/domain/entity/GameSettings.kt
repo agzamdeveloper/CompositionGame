792f6fa53039b087f9f9988d05f4b6f4eb05a803
Agzam <agzamasadullaev@gmail.com>
2024-06-01 15:54:18 +0500

    using parcelable instead of serializable

--- app/src/main/java/com/bionickhand/composition/domain/entity/GameSettings.kt
+++ app/src/main/java/com/bionickhand/composition/domain/entity/GameSettings.kt
@@ -1,10 +1,12 @@
 package com.bionickhand.composition.domain.entity
 
-import java.io.Serializable
+import android.os.Parcelable
+import kotlinx.parcelize.Parcelize
 
+@Parcelize
 data class GameSettings(
     val maxSumValue: Int,
     val minCountOfRightAnswers: Int,
     val mainPercentOfRightAnswers: Int,
     val gameTimeInSeconds: Int
-) : Serializable
+) : Parcelable
