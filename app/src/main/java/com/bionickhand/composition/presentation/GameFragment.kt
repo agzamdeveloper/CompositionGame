127751bb1c2abeb6a361061b8c70e2d672402b94
Agzam <agzamasadullaev@gmail.com>
2024-06-06 17:11:47 +0500

    create game view model factory

--- app/src/main/java/com/bionickhand/composition/presentation/GameFragment.kt
+++ app/src/main/java/com/bionickhand/composition/presentation/GameFragment.kt
@@ -20,7 +20,7 @@ class GameFragment : Fragment() {
     private val viewModel by lazy {
         ViewModelProvider(
             this,
-            ViewModelProvider.AndroidViewModelFactory.getInstance(requireActivity().application)
+            GameViewModelFactory(level, requireActivity().application)
         )[GameViewModel::class.java]
     }
 
@@ -57,7 +57,6 @@ class GameFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
         observeViewModel()
         setClickListenersToOptions()
-        viewModel.startGame(level)
     }
 
     override fun onDestroy() {
